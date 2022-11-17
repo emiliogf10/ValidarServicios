131e2bfdecd3f244e1e0aef40e2d4993760799e5
emiliogf10 <113925988+emiliogf10@users.noreply.github.com>
2022-11-17 19:29:13 +0100

    Add files via upload

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Ejercicio4
+
+{
+    
+    internal class Program
+    {
+        static int cont = 0;
+        static readonly object l = new object();
+        static bool finished = false;
+
+        static void increase()
+        {
+           
+                while (!finished)
+                {
+                    lock (l)
+                    {
+                        if (!finished)
+                        {
+                            cont++;
+                            Console.WriteLine("Increase: {0}", cont);
+                            
+                             if (cont == 1000)
+                             {
+                                finished = true;
+                             }
+                        }
+
+
+                    }
+
+                
+                
+                }
+            
+        }
+
+        static void decrease()
+        {
+                while (!finished)
+                {
+                    lock (l)
+                    {
+                        if (!finished)
+                        {
+                            cont--;
+                            Console.WriteLine("Decrease: {0}", cont);
+                        }
+
+
+
+
+                    }
+
+                if (cont == -1000){
+                    finished = true;
+                }
+                
+            }
+
+           
+            
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(increase);
+            Thread thread2 = new Thread(decrease);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            if(cont == 1000)
+            {
+                Console.WriteLine("Thread 1 wins");
+            }
+            else
+            {
+                Console.WriteLine("Thread 2 wins");
+            }
+
+
+
+
+        }
+    }
+}
